d9600e74c46139f617de28aaded1c9f46b841848
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-14 10:15:30 +0700

    First Commit

---
+++ src/test/java/id/co/indivara/perpustakaan/PerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.indivara.perpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
