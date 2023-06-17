c5fa9b10f9cfbfdb1d0749ef55e7ac120be66d1e
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-17 19:33:07 +0700

    Change: disable csrf

--- src/main/java/id/co/indivara/perpustakaan/configs/SecurityConfig.java
+++ src/main/java/id/co/indivara/perpustakaan/configs/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
+                .csrf().disable()
                 .httpBasic();
     }
 
