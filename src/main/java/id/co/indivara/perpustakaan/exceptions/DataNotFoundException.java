20a32a7d26f15eadb60ab6c594813eb43b939e78
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-17 19:35:57 +0700

    Add: Exception for Data not Found

---
+++ src/main/java/id/co/indivara/perpustakaan/exceptions/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package id.co.indivara.perpustakaan.exceptions;
+
+public class DataNotFoundException extends RuntimeException{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
