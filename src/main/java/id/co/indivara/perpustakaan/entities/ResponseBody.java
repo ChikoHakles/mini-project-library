eb9bef7ee02279a271c2ff174bd6d7433c5c0843
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-17 19:34:59 +0700

    Change: ResponseBody now use @Builder and another lombok object annotation

--- src/main/java/id/co/indivara/perpustakaan/entities/ResponseBody.java
+++ src/main/java/id/co/indivara/perpustakaan/entities/ResponseBody.java
@@ -1,55 +1,18 @@
 package id.co.indivara.perpustakaan.entities;
 
-import java.util.ArrayList;
-import java.util.Date;
+import lombok.*;
 
+import java.util.Date;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class ResponseBody<T> {
     private final Date timestamp = new Date();
     private int status;
     private String message;
-    private ArrayList<String> error;
+    @Singular private List<String> errors;
     private T data;
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public ArrayList<String> getError() {
-        return error;
-    }
-
-    public void setError(ArrayList<String> error) {
-        this.error = error;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public ResponseBody(int status, String message, ArrayList<String> error, T data) {
-        this.status = status;
-        this.message = message;
-        this.error = error;
-        this.data = data;
-    }
 }
