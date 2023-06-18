72446b32712aa9a41450efd9b22107a471d0be0f
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-18 16:22:26 +0700

    Add : Utility class (by now it containing copyNonNullField for copying field value form one object to other, and getNullPropertiesName returning the field name that have null value)

---
+++ src/main/java/id/co/indivara/perpustakaan/utils/Utility.java
@@ -0,0 +1,26 @@
+package id.co.indivara.perpustakaan.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.FeatureDescriptor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Utility {
+    public static void copyNonNullField(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertiesName(source));
+    }
+
+    public static String[] getNullPropertiesName(Object source) {
+        BeanWrapper src = new BeanWrapperImpl(source);
+        String idField = source.getClass().getSimpleName().toLowerCase() + "Id";
+        return Stream.of(src.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> src.getPropertyValue(propertyName) == null || src.getPropertyDescriptor(propertyName).getName().equals(idField))
+                .toArray(String[]::new);
+    }
+}
