5dfeee41fe339a6a52048a62886fccc5b95f85eb
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-18 16:24:05 +0700

    Add : [PUT] /books/{id} and [DELETE] /books/{id}, Fix : RequestBody now Nullable and will be checked in Service instead

--- src/main/java/id/co/indivara/perpustakaan/controllers/BookController.java
+++ src/main/java/id/co/indivara/perpustakaan/controllers/BookController.java
@@ -6,12 +6,11 @@ import id.co.indivara.perpustakaan.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -39,10 +38,27 @@ public class BookController {
     }
 
     @PostMapping("/books")
-    ResponseEntity<ResponseBody<Book>> saveBook(@Valid @RequestBody Book book) {
+    ResponseEntity<ResponseBody<Book>> saveBook(@Valid @Nullable @RequestBody Book book) {
         return new ResponseEntity<>(
                 new ResponseBody<>(HttpStatus.CREATED.value(), "Data Created", null, bookService.saveBook(book)),
                 HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping("/books/{id}")
+    ResponseEntity<ResponseBody<Book>> updateBook(@PathVariable(name = "id") Integer id, @Nullable @RequestBody Book book) {
+        return new ResponseEntity<>(
+                new ResponseBody<>(HttpStatus.ACCEPTED.value(), "Data Updated", null, bookService.updateBook(id, book)),
+                HttpStatus.ACCEPTED
+        );
+    }
+
+    @DeleteMapping("/books/{id}")
+    ResponseEntity<ResponseBody<Object>> deleteBook(@PathVariable(name = "id") Integer id) {
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(
+                new ResponseBody<>(HttpStatus.OK.value(), "Data Deleted", null, null),
+                HttpStatus.OK
         );
     }
 }
