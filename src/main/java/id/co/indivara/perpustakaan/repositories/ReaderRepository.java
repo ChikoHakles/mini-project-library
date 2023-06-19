39bb2ec5f237cd10d0ad656850d346199c3fbe55
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-19 11:01:26 +0700

    Fix : Add @Repository

--- src/main/java/id/co/indivara/perpustakaan/repositories/ReaderRepository.java
+++ src/main/java/id/co/indivara/perpustakaan/repositories/ReaderRepository.java
@@ -2,7 +2,9 @@ package id.co.indivara.perpustakaan.repositories;
 
 import id.co.indivara.perpustakaan.entities.Reader;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReaderRepository extends CrudRepository<Reader, Integer> {
 
 }
