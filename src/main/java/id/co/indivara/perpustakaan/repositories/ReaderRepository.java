747a487ed231a4013922a776d1210738056ada25
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-19 10:56:33 +0700

    Add : ReaderRepository

---
+++ src/main/java/id/co/indivara/perpustakaan/repositories/ReaderRepository.java
@@ -0,0 +1,8 @@
+package id.co.indivara.perpustakaan.repositories;
+
+import id.co.indivara.perpustakaan.entities.Reader;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReaderRepository extends CrudRepository<Reader, Integer> {
+
+}
