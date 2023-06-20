bb8333cecd497bbe285c5f5d47dfbc4596509f2b
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-20 22:41:26 +0700

    Change : Add @Repository

--- src/main/java/id/co/indivara/perpustakaan/repositories/CategoryRepository.java
+++ src/main/java/id/co/indivara/perpustakaan/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package id.co.indivara.perpustakaan.repositories;
 
 import id.co.indivara.perpustakaan.entities.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, String> {
 }
