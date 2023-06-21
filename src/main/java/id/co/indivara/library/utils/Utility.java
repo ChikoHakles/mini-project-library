c3ae1b20ff1576e7154e4cba6d652cd5425c8518
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-21 16:41:13 +0700

    Add : Method for generate Transaction Code

--- src/main/java/id/co/indivara/library/utils/Utility.java
+++ src/main/java/id/co/indivara/library/utils/Utility.java
@@ -5,6 +5,9 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.FeatureDescriptor;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class Utility {
@@ -19,5 +22,25 @@ public class Utility {
                 .map(FeatureDescriptor::getName)
                 .filter(propertyName -> src.getPropertyValue(propertyName) == null || src.getPropertyDescriptor(propertyName).getName().equals(idField))
                 .toArray(String[]::new);
+    }
+
+    public static String generateCode(TransactionType type, LocalDateTime localDateTime) {
+        return String.format(
+                "%c%2d%2d%2d%s",
+                type.toString().charAt(0),
+                localDateTime.getDayOfMonth(),
+                localDateTime.getMonth().getValue(),
+                localDateTime.getYear(),
+                randomChars(3)
+                );
+    }
+
+    public static String randomChars(int loops) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < loops; i++) {
+            stringBuilder.append(random.nextInt(26) + 65);
+        }
+        return stringBuilder.toString();
     }
 }
