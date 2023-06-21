2387a6c53511be63cb7fdde6bbe46e73b7bf1ac4
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-21 16:50:48 +0700

    Change : Add @Repository

--- src/main/java/id/co/indivara/library/repositories/BorrowRepository.java
+++ src/main/java/id/co/indivara/library/repositories/BorrowRepository.java
@@ -4,10 +4,12 @@ import id.co.indivara.library.entities.Book;
 import id.co.indivara.library.entities.Borrow;
 import id.co.indivara.library.entities.Reader;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface BorrowRepository extends CrudRepository<Borrow, UUID> {
     List<Borrow> findAllByBook(Book book);
     List<Borrow> findAllByReader(Reader reader);
--- src/main/java/id/co/indivara/library/repositories/WishlistRepository.java
+++ src/main/java/id/co/indivara/library/repositories/WishlistRepository.java
@@ -4,9 +4,11 @@ import id.co.indivara.library.entities.Book;
 import id.co.indivara.library.entities.Reader;
 import id.co.indivara.library.entities.Wishlist;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+@Repository
 public interface WishlistRepository extends CrudRepository<Wishlist, Integer> {
     ArrayList<Wishlist> findAllByBook(Book book);
     ArrayList<Wishlist> findAllByReader(Reader reader);
