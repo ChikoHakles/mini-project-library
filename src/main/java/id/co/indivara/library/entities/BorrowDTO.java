0ee64180e78bd6d4f8e11b87d6d659e84dfa66a2
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-21 23:56:41 +0700

    Add : Borrow DTO

---
+++ src/main/java/id/co/indivara/library/entities/BorrowDTO.java
@@ -0,0 +1,14 @@
+package id.co.indivara.library.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowDTO {
+    private Integer borrowId;
+    private Integer bookId;
+    private Integer readerId;
+}
