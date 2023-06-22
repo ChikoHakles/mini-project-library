c30ba2f78eb85bdba8df795438b6faa49ad29847
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-22 12:40:38 +0700

    Change : Add @Repository

--- src/main/java/id/co/indivara/library/repositories/ReturnRepository.java
+++ src/main/java/id/co/indivara/library/repositories/ReturnRepository.java
@@ -3,9 +3,11 @@ package id.co.indivara.library.repositories;
 import id.co.indivara.library.entities.Borrow;
 import id.co.indivara.library.entities.Return;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ReturnRepository extends CrudRepository<Return, UUID> {
     Return findByBorrow(Borrow borrow);
     Return findByReturnCode(String code);
