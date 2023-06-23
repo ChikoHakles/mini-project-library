b1c1c95c74fbde8edf1326a133a1b9911e32d927
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-23 23:37:56 +0700

    Add : Librarian and Reader, account, and its permission

--- src/main/java/id/co/indivara/library/configs/SecurityConfig.java
+++ src/main/java/id/co/indivara/library/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package id.co.indivara.library.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,18 +14,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/**").authenticated()
-                .anyRequest().permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "LIBRARIAN", "READER")
+                .antMatchers(HttpMethod.POST, "/books").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.PUT, "/books/**").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.DELETE, "/books/**").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.POST, "/readers").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.PUT, "/readers/**").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.DELETE, "/readers/**").hasAnyRole("ADMIN", "LIBRARIAN")
+                .antMatchers(HttpMethod.POST, "/wishlists").hasAnyRole("LIBRARIAN")
+                .antMatchers(HttpMethod.POST, "/borrows").hasAnyRole("LIBRARIAN")
+                .antMatchers(HttpMethod.POST, "/returns").hasAnyRole("LIBRARIAN")
+                .anyRequest().authenticated()
                 .and()
-                .csrf().disable()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("{noop}password")
-                .roles("ADMIN");
+                .withUser("admin").password("{noop}admin").roles("ADMIN")
+                .and()
+                .withUser("librarian").password("{noop}librarian").roles("LIBRARIAN")
+                .and()
+                .withUser("reader").password("{noop}reader").roles("READER");
     }
 }
