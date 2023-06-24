155f64060b6941d30c138c1cb58dcc56ec56455a
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-24 22:30:11 +0700

    Add : @Builder

--- src/main/java/id/co/indivara/library/entities/BorrowDTO.java
+++ src/main/java/id/co/indivara/library/entities/BorrowDTO.java
@@ -1,10 +1,12 @@
 package id.co.indivara.library.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BorrowDTO {
--- src/main/java/id/co/indivara/library/entities/WishlistDTO.java
+++ src/main/java/id/co/indivara/library/entities/WishlistDTO.java
@@ -1,10 +1,12 @@
 package id.co.indivara.library.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class WishlistDTO {
