34aad4fd4d696f68a777fbf75dec0cbfd35c1173
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-24 17:24:09 +0700

    Delete : Anything About BookCategory

--- src/main/java/id/co/indivara/library/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package id.co.indivara.library.repositories;
-
-import id.co.indivara.library.entities.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends CrudRepository<Category, String> {
-}
