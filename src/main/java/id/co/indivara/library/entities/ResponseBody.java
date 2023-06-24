506ed56c6ad75e8eaa060a7c94b5fd0b4cc98185
ChikoHakles <2014484.chikopratama@gmail.com>
2023-06-24 17:26:30 +0700

    Add : NoArgsConstructor at ResponseBody

--- src/main/java/id/co/indivara/library/entities/ResponseBody.java
+++ src/main/java/id/co/indivara/library/entities/ResponseBody.java
@@ -8,11 +8,12 @@ import java.util.List;
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ResponseBody<T> {
     private final Date timestamp = new Date();
     private int status;
     private String message;
-    @Singular private List<String> errors;
+    private List<String> errors;
     private T data;
 }
